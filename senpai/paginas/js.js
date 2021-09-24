8e1b6a6b75b66b2d95d467475fb4e237179bb4dc
Cristian <87790623+CrisGonzalo@users.noreply.github.com>
2021-09-24 13:57:24 -0300

    Add files via upload

---
+++ senpai/paginas/js.js
@@ -0,0 +1,33 @@
+ 
+function abrir (){ 
+    document.getElementById ("vent1").style.display="block";
+    }
+    function cerrar1 (){
+    document.getElementById ("vent1").style.display="none";
+    } 
+    function abrir2 (){ 
+    document.getElementById ("vent2").style.display="block";
+    }
+    function cerrar2 (){
+    document.getElementById ("vent2").style.display="none";
+    } 
+    function abrir3 (){ 
+    document.getElementById ("vent3").style.display="block";
+    }
+    function cerrar3 (){
+    document.getElementById ("vent3").style.display="none";
+    } 
+    function abrir4 (){ 
+    document.getElementById ("vent4").style.display="block";
+    }
+    function cerrar4 (){
+    document.getElementById ("vent4").style.display="none";
+    } 
+
+var getData = function (){
+
+   var name = document.getElementById("name").value;
+    console.log(name+"");
+ 
+
+}
