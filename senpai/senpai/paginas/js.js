fda5ef76708a18babc36167cf8d6d0469c64d8ef
Cristian <87790623+CrisGonzalo@users.noreply.github.com>
2021-09-29 17:45:41 -0300

    Add files via upload

---
+++ senpai/senpai/paginas/js.js
@@ -0,0 +1,50 @@
+ 
+// * esta hoja de javascript pertenece a la hoja nuevaempresa. no esta terminada.
+
+
+function abrir (){ 
+    document.getElementById ("vent1").style.display="block";
+    }
+    function cerrar1 (){
+    document.getElementById ("vent1").style.display="none";
+    } 
+    function abrir2 (){ 
+    document.getElementById ("vent2").style.display="block";
+    }
+    function cerrar2 (){
+    document.getElementById ("vent2").style.display="none";
+    } 
+    function abrir3 (){ 
+    document.getElementById ("vent3").style.display="block";
+    }
+    function cerrar3 (){
+    document.getElementById ("vent3").style.display="none";
+    } 
+    function abrir4 (){ 
+    document.getElementById ("vent4").style.display="block";
+    }
+    function cerrar4 (){
+    document.getElementById ("vent4").style.display="none";
+    } 
+
+   var getData = function (){
+   
+   var name1 = document.getElementById("name1").value;
+   var name2 = document.getElementById("name2").value;
+   var name3 = document.getElementById("name3").value;
+   var name4 = document.getElementById("name4").value;
+   var name5 = document.getElementById("name5").value;
+   var name6 = document.getElementById("name6").value;
+   var name7 = document.getElementById("name7").value;
+   var name8 = document.getElementById("name8").value;
+   var name9 = document.getElementById("name9").value;
+   var name10 = document.getElementById("name10").value;
+   var name11 = document.getElementById("name11").value;
+   var name12 = document.getElementById("name12").value;
+   var name13 = document.getElementById("name13").value;
+   var name14 = document.getElementById("name14").value;
+
+ console.log (name1&name2&name3&name4&name5&name6&name7&name8&name9&name10&name11&name12&name13&name14)
+
+
+}
